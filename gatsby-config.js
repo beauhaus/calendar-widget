5d2024e39607756696ecf101a0a3190da80f79f3
Beau Jackson <beau.html@gmail.com>
2020-02-28 16:14:43 -0500

    landscape test 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
             background_color: `#f7f0eb`,
             theme_color: `#a2466c`,
             display: `standalone`,
-            icon: siteMetadata.favicon
+            // icon: siteMetadata.favicon
           },
         },
         `gatsby-plugin-offline`,
